50952eca9154dbe80c0c6794fceb57726afc0f8a
jimbae <jimbae@kakao.com>
2019-12-10 23:30:35 +0900

    단일 책임 원칙 공부

---
+++ src/objectOriented/single_responsibility_principle/DivideOperation.java
@@ -0,0 +1,11 @@
+package objectOriented.single_responsibility_principle;
+
+/**
+ * DivideOperation
+ */
+public class DivideOperation {
+
+    public int operate(int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+}
