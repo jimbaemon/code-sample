684fadbda35d143ecc58e66c31bfe87a3dc615bc
jimbae <jimbae@kakao.com>
2019-12-21 22:23:04 +0900

    프로토타입

---
+++ src/pattern/prototype/DivideOperationPrototype.java
@@ -0,0 +1,31 @@
+package pattern.prototype;
+
+/**
+ * SubsttactOperationPrototype
+ */
+public class DivideOperationPrototype extends AbstractOperationPrototype{
+
+    public DivideOperationPrototype(){
+        super();
+    }
+
+    public DivideOperationPrototype(AbstractOperationPrototype operation){
+        super(operation);
+    }
+
+    @Override
+    public AbstractOperationPrototype getClone(){
+        return new DivideOperationPrototype(this);
+    }
+
+    @Override
+    protected int getAnswer(int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+
+    @Override
+    protected String getOperator(){
+        return "/";
+    }
+    
+}
