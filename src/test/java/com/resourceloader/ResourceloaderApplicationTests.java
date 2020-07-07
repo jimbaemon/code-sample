f31dbb1edeaf44bba3141aaffb2283954bfec0f1
jimbae <jimbae@kakao.com>
2020-07-07 22:01:13 +0900

    Initial commit

---
+++ src/test/java/com/resourceloader/ResourceloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourceloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
