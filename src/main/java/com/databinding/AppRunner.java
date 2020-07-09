f990f120340c1ad8772cbc94f9a4470ed1d2a3c0
jimbae <jimbae@kakao.com>
2020-07-09 18:02:36 +0900

    추가

---
+++ src/main/java/com/databinding/AppRunner.java
@@ -0,0 +1,21 @@
+package com.databinding;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Autowired
+    ConversionService conversionService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(conversionService); // INFO : 등록된 컨버전 서비스 조회
+
+        System.out.println(conversionService.getClass().toString());
+    }
+}
