b224e5e203b85d84cc65094665f04daf9d95f7c1
jimbae <jimbae@kakao.com>
2020-07-09 16:38:59 +0900

    Initial commit

---
+++ src/main/java/com/databinding/EventController.java
@@ -0,0 +1,26 @@
+package com.databinding;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+
+    /*
+    * INFO : Controller 진입시 사용할 바인더 지정 
+    */
+    @InitBinder
+    public void init(WebDataBinder webDataBinder){
+        webDataBinder.registerCustomEditor(Event.class, new EventEditor());
+    }
+
+    @GetMapping("/event/{event}")
+    public String getEvent(@PathVariable Event event){
+        System.out.println(event);
+        return event.getId().toString();
+    }
+
+}
---
+++ src/test/java/com/databinding/EventControllerTest.java
@@ -0,0 +1,28 @@
+package com.databinding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getTest() throws Exception {
+        mockMvc.perform(get("/event/1")) //INFO : Get 방식으로 요청
+                .andExpect(status().isOk()) //INFO : 상태코드가 200 인가
+                .andExpect(content().string("1"));
+    }
+
+}
