156ff3553ea2bd08df16d9f97d2d5836b7536d32
jimbae <jimforgame@gmail.com>
2020-09-19 23:54:13 +0900

    Initial commit

---
+++ src/main/java/com/hellojpa/JpaMain.java
@@ -0,0 +1,45 @@
+package com.hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+
+            System.out.println("TeamName = "+findTeam.getName());
+
+
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            //code
+            em.close();
+        }
+        emf.close();
+    }
+}
