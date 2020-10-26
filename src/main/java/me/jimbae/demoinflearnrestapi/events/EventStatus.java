46bfcdcf4b08684c85be617a26057394fb0a5575
jimbaemon <jimbae@kakao.com>
2020-10-26 23:15:05 +0900

    Initial commit

---
+++ src/main/java/me/jimbae/demoinflearnrestapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package me.jimbae.demoinflearnrestapi.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/me/jimbae/demoinflearnrestapi/events/EventTest.java
@@ -0,0 +1,31 @@
+package me.jimbae.demoinflearnrestapi.events;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        Event event = new Event();
+        String name = "Event";
+        event.setName(name);
+        String description = "Spring";
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
