df482c42c2d7690e9a0081b6c572cf4767945746
jimbaemon <jimbae@kakao.com>
2020-10-28 00:12:49 +0900

    201 테스트

--- src/main/java/me/jimbae/demoinflearnrestapi/events/Event.java
+++ src/main/java/me/jimbae/demoinflearnrestapi/events/Event.java
@@ -2,25 +2,27 @@ package me.jimbae.demoinflearnrestapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@EqualsAndHashCode(of = "id")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
-    private String name;
-    private String description;
-    private LocalDateTime beginEnrollmentDateTime;
-    private LocalDateTime closeEnrollmentDateTime;
-    private LocalDateTime beginEventDateTime;
-    private LocalDateTime endEventDateTime;
+    @Id @GeneratedValue
+    private int id;
+    private String name; //이름
+    private String description; //설명
+    private LocalDateTime beginEnrollmentDateTime; //등록시작일시
+    private LocalDateTime closeEnrollmentDateTime; //등록마감일시
+    private LocalDateTime beginEventDateTime; //이벤트 시작일시
+    private LocalDateTime endEventDateTime; //이벤트 종료일시
     private String location; // (optional) 이게 없으면 온라인 모임
-    private int basePrice; // (optional)
-    private int maxPrice; // (optional)
-    private int limitOfEnrollment;
+    private int basePrice; // (optional) //참가비
+    private int maxPrice; // (optional) // 경매비 0이면 무료 or 무제한
+    private int limitOfEnrollment; //등록 제한
     private boolean offline;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 
