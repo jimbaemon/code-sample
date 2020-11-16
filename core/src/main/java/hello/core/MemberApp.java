bd13470214c7a63a2674f4cda5fdfddd9e12036b
jimbaemon <jimbae@kakao.com>
2020-11-17 00:08:44 +0900

    feat : 테스트 코드 추가 & 메모 추가

---
+++ core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.member.Grade;
+import hello.member.Member;
+import hello.member.MemberService;
+import hello.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+
+    }
+}
