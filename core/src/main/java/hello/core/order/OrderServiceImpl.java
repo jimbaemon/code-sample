00fa5a09f655b78c8a0895f9f12c6b647fcb283e
jimbaemon <jimbae@kakao.com>
2020-12-06 23:39:08 +0900

    feat : 주입하는 빈이 여러개일때 처리 방법 메소드명 맞추기 @Qualifier @Primary
    @Qualifier 가 컴파일에서 검수가능하게끔 애노테이션으로 만들기

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -8,10 +9,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor //final 이 붙은 애들을 생성자로 만들어줌
+//@RequiredArgsConstructor //final 이 붙은 애들을 생성자로 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     //@Autowired //필드 Autowired 시 테스트에 Repository 할당 방법이 없다.....
@@ -19,6 +21,30 @@ public class OrderServiceImpl implements OrderService{
     //private  DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //@Autowired
     private final DiscountPolicy discountPolicy;
+
+/*    
+    //같은빈이 2개 이상일떄 1번 이름으로 매핑
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
+*/
+
+    //같은빈이 2개 이상일떄 2번 @Qualifier로 매핑
+/*
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+*/
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
 
 /*    
     //수정자 주입
@@ -34,15 +60,8 @@ public class OrderServiceImpl implements OrderService{
         this.discountPolicy = discountPolicy;
     }*/
 
-/*
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-*/
+
+
 
 /*    @Autowired // 일반 메서드에서도 Autowired 사용 가능
     public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
