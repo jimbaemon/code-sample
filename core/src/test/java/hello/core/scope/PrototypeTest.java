8835f4711780cec6425b2158cd87e0221fb30068
jimbaemon <jimbae@kakao.com>
2020-12-10 23:23:27 +0900

    feat : 프로토타입 스코프, 싱클톤 타입 스코프 비교

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,41 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+        System.out.println("find PrototypeBean1");
+        ProtoTypeBean protoTypeBean1 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("find PrototypeBean2");
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("protoTypeBean1 = " + protoTypeBean1);
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+
+        Assertions.assertThat(protoTypeBean1).isNotSameAs(protoTypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+
+}
