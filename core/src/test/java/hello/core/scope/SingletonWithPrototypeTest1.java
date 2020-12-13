8e0711ffacdf311815d32b4ef1f1c700a90f3cdb
jimbaemon <jimbae@kakao.com>
2020-12-13 16:42:40 +0900

    feat : JSR-330 표준 Provider 을 이용한 DL 구현

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,23 +37,27 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
         //assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
+        //private final PrototypeBean prototypeBean;
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;
 
         /*@Autowired
         ApplicationContext applicationContext;*/
 
-        @Autowired
+/*        @Autowired
         public ClientBean(PrototypeBean prototypeBean){
             this.prototypeBean = prototypeBean;
-        }
+        }*/
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
             //PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCount();
