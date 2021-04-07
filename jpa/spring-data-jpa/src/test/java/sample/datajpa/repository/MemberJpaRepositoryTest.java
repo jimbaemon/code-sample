7415f1c82484b14587bf262b33b664a93804f9e2
jimbaemon <jimbae@kakao.com>
2021-04-07 22:48:45 +0900

    fix : spring data jpa 로 변경

--- jpa/spring-data-jpa/src/test/java/sample/datajpa/repository/MemberJpaRepositoryTest.java
+++ jpa/spring-data-jpa/src/test/java/sample/datajpa/repository/MemberJpaRepositoryTest.java
@@ -61,4 +61,6 @@ class MemberJpaRepositoryTest {
 		long deletedCount = memberJpaRepository.count();
 		assertThat(deletedCount).isEqualTo(0);
 	}
+
+
 }
--- jpa/spring-data-jpa/src/test/java/sample/datajpa/repository/MemberRepositoryTest.java
+++ jpa/spring-data-jpa/src/test/java/sample/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,8 @@ package sample.datajpa.repository;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,5 +31,33 @@ class MemberRepositoryTest {
 		assertThat(findMember.getId()).isEqualTo(savedMember.getId());
 		assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
 		assertThat(findMember).isEqualTo(savedMember);
+	}
+
+	@Test
+	public void basicCRUD(){
+		Member member1 = new Member("member1");
+		Member member2 = new Member("member2");
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		//단건 조회
+		Member findMember1 = memberRepository.findById(member1.getId()).get();
+		Member findMember2 = memberRepository.findById(member2.getId()).get();
+		assertThat(findMember1).isEqualTo(member1);
+		assertThat(findMember2).isEqualTo(member2);
+
+		//리스트 조회 검증
+		List<Member> all = memberRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+
+		//카운트
+		long count = memberRepository.count();
+
+		//삭제
+		memberRepository.delete(member1);
+		memberRepository.delete(member2);
+
+		long deletedCount = memberRepository.count();
+		assertThat(deletedCount).isEqualTo(0);
 	}
 }
